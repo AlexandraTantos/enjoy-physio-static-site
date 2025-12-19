258dbb0ad1deba26824a71a566c246154e67ed93
Tanțoș Alexandra-Bianca <114863064+AlexandraTantos@users.noreply.github.com>
2025-12-19 17:25:27 +0200

    Add JS : calculate discounted price and dynamically add promo packages on home page

---
+++ data.js
@@ -0,0 +1,50 @@
+export const packages =[
+    {
+        id:1,
+        image:"./images/fullfunctionality.png",
+        title:"The Full Functionality Pack",
+        description:"Our most comprehensive program for lasting results and completenmobility. Designed to restore strength, flexibility, and movement for everyday life.",
+        sessions:"10 Combined Sessions (Mix of Electrotherapy & Exercise) + 2 Free Maintenance Sessions",
+        price: 400
+    },
+{
+        id:2,
+        image:"./images/office.png",
+        title:"The Chronic Relief Pack (For Office Workers)",
+        description:"Break free from chronic neck and lower back discomfort.Targeted therapy helps reduce pain, improve posture, and restore balance.",
+        sessions:"5 Manual Therapy Sessions + Personalized Ergonomics Guide",
+        price: 200
+    },
+    {
+        id:3,
+        image:"./images/ski.png",
+        title:"The Ski Prep Promo Package",
+        description:"Hit the mountain with confidence. Your best run starts with the best prep. Performance-focused training prepares your body for strength, control, and endurance.",
+        sessions:"1 Initial Stability Assessment + 12 Ski-Specific Exercise Therapy Sessions + Take-Home Stretching Guide",
+        price: 530
+    },
+    {
+        id:4,
+        image:"./images/wellness.png",
+        title:"The Wellness Pack (Maintenance & Prevention)",
+        description:"Invest in long-term health and prevent future injuries.Proactive care builds resilience and supports an active, pain-free lifestyle.",
+        sessions:"15 Sessions focused on stretching and core strength + Free Physio Check-up",
+        price: 600
+    },
+    {
+        id:5,
+        image:"./images/acl-knee.jpg",
+        title:"ACL Recovery for Athletes",
+        description:"Get back on the field stronger and safer. Our sport-specific rehab builds strength, agility, and control for your best performance yet.",
+        sessions:"20 Electrotherapy & Sport-Specific Conditioning + 8 Free Performance Maintenance Sessions",
+        price: 650
+    },
+    {
+        id:6,
+        image:"./images/ankle-injury.jpg",
+        title:"Achilles Tendon Rehab for Athletes",
+        description:"Recover your Achilles with care. Gradual loading and targeted training build strength, flexibility, and explosive power for your best performance.",
+        sessions:"24 Electrotherapy & Progressive Sport Conditioning + 8 Free Support & Performance Sessions",
+        price: 800
+    },
+]
--- index.js
+++ index.js
@@ -1,26 +1,92 @@
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-const wrap = document.querySelector('.carousel-items');
-const imgs = document.querySelectorAll('.carousel-items img');
+import { packages } from "./data.js";
+
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const wrap = document.querySelector(".carousel-items");
+const imgs = document.querySelectorAll(".carousel-items img");
+const packagesSection = document.querySelector("#promo");
+const discountContainer = document.querySelector("#discount");
+
+if(packagesSection){
+document.addEventListener("DOMContentLoaded", function () {
+  displayPackages(packages, packagesSection);
+});
+}
+
 
 let idx = 0;
 
 function showImg() {
-    if (idx >= imgs.length) idx = 0;
-    if (idx < 0) idx = imgs.length - 1;
-    wrap.style.transform = `translateX(-${idx * 100}%)`;
+  if (idx >= imgs.length) {
+    idx = 0;
+  }
+  if (idx < 0) {
+    idx = imgs.length - 1;
+  }
+  wrap.style.transform = `translateX(-${idx * 100}%)`;
 }
-
-next.addEventListener('click', () => {
+if (next) {
+  next.addEventListener("click", () => {
     idx++;
     showImg();
-});
+  });
+}
 
-prev.addEventListener('click', () => {
+if (prev) {
+  prev.addEventListener("click", () => {
     idx--;
     showImg();
-});
+  });
+}
+if (wrap) {
+  showImg();
+}
 
+function displayPackages(packages, container) {
+  const promoPackage = packages.map((item) => {
+    return `<article class="section__card">
+            <div class="section__card__container">
+              <img
+                src="${item.image}"
+                class="section__card__img"
+                alt="${item.title}"
+              />
+            </div>
+            <div class="section__card__info">
+              <div class="section__card__title">
+                <h4>${item.title}</h4>
+              </div>
+              <p>
+                ${item.description}
+              </p>
+              <div class="section__card__footer">
+                <p>
+                  ${item.sessions}
+                </p>
+                <p>From $${item.price}</p>
+              </div>
+            </div>
+          </article>`;
+  });
+  if(container){
+    container.innerHTML = promoPackage.join("");
+  }
+}
 
+if(discountContainer){
+    const exercisePrice = 45;
+    const electroPrice = 30;
+    const massagePrice = 45;
+    const discountPercentage = 20;
 
-showImg();
+    function displayDiscount(exercisePrice,electroPrice,massagePrice,discountPercentage){
+        const totalPrice = exercisePrice + electroPrice + massagePrice;
+        const discountPrice = totalPrice - (totalPrice*discountPercentage/100);
+        return discountPrice;
+    }
+
+    document.addEventListener("DOMContentLoaded", function (){
+        const discountedPrice = displayDiscount(exercisePrice,electroPrice,massagePrice,discountPercentage);
+        discountContainer.innerHTML = discountedPrice;
+    })
+}
